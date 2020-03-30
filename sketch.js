c1128b13e1131b97990e9c9b35ce4515c2c032f9
miguel-kjh <miguelangel.kjh@gmail.com>
2020-03-30 18:26:33 +0100

    ability to draw done

--- sketch.js
+++ sketch.js
@@ -1,14 +1,61 @@
-var presed = false;
+const x = 1000;
+const y = 1000;
+var drawings;
+var thickness;
+var paintColor;
 
 function setup() {
-    createCanvas(400, 500);
+    createCanvas(x, y);
+    drawings = [];
+    thickness = $("#sliderWidth").val();
+    paintColor = color(0);
 }
   
 function draw() {
     background(220);
-    if(presed)line(mouseX, mouseY, 0, 0);
+    paint();
 }
 
-function mouseClicked() {
-    presed = !presed;
+function paint() {
+    drawings.forEach(element => {
+        if (element.points.length == 1) {
+            const initialPoint = element.points[0]
+            noFill();
+            fill(element.color);
+            strokeWeight(element.width);
+            point(initialPoint[0], initialPoint[1]);
+            return;
+        }
+        for (let index = 0; index < element.points.length-1; index++) {
+            const initialPoint = element.points[index];
+            const secondPoint  = element.points[index+1];
+            noFill();
+            fill(element.color);
+            strokeWeight(element.width);
+            line(initialPoint[0], initialPoint[1], secondPoint[0], secondPoint[1]);
+            
+        }
+    });
 }
+
+function mousePressed(){
+    drawings.push({
+        "points": [[mouseX,mouseY]],
+        "width": thickness,
+        "color": paintColor
+    });
+}
+
+function mouseDragged(){
+    drawings[drawings.length - 1].points.push([mouseX,mouseY]);
+}
+
+/*function mouseReleased(){
+    console.log("dejar");
+}*/
+
+$(document).ready(function(){
+    $("#sliderWidth").change(function(){
+        thickness = $(this).val();
+    });
+});  
