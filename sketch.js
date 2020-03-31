5bb2a5d986878756701a90f8691126c533b7ed6b
miguel-kjh <miguelangel.kjh@gmail.com>
2020-03-31 11:20:24 +0100

    colors done

--- sketch.js
+++ sketch.js
@@ -6,22 +6,28 @@ var paintColor;
 
 function setup() {
     createCanvas(x, y);
-    drawings = [];
-    thickness = $("#sliderWidth").val();
-    paintColor = color(0);
+    drawings   = [];
+    paintColor = chooseColor();
+    thickness  = $("#sliderWidth").val();
 }
   
 function draw() {
     background(220);
+    paintMark();
     paint();
+}
+
+function paintMark() {
+    stroke(paintColor);
+    strokeWeight(thickness);
+    point(mouseX, mouseY);
 }
 
 function paint() {
     drawings.forEach(element => {
         if (element.points.length == 1) {
             const initialPoint = element.points[0]
-            noFill();
-            fill(element.color);
+            stroke(element.color);
             strokeWeight(element.width);
             point(initialPoint[0], initialPoint[1]);
             return;
@@ -29,8 +35,7 @@ function paint() {
         for (let index = 0; index < element.points.length-1; index++) {
             const initialPoint = element.points[index];
             const secondPoint  = element.points[index+1];
-            noFill();
-            fill(element.color);
+            stroke(element.color);
             strokeWeight(element.width);
             line(initialPoint[0], initialPoint[1], secondPoint[0], secondPoint[1]);
             
@@ -54,8 +59,26 @@ function mouseDragged(){
     console.log("dejar");
 }*/
 
+function chooseColor() {
+    string = $("#colors").val();
+    switch (string) {
+        case "green":
+            return color(40, 227, 37);
+        case "blue":
+            return color( 7, 143, 243 );
+        case "red":
+            return color( 228, 29, 29 );
+        default:
+            return color(0);
+    }
+}
+
 $(document).ready(function(){
     $("#sliderWidth").change(function(){
         thickness = $(this).val();
+    });
+
+    $("select").change(function () { 
+        paintColor = chooseColor()    
     });
 });  
