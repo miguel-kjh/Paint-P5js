672d5f41f7573f52cabe71052482be92a0faa209
miguel-kjh <miguelangel.kjh@gmail.com>
2020-04-01 11:47:57 +0100

    rubber initial

--- sketch.js
+++ sketch.js
@@ -1,19 +1,33 @@
 const x = 1000;
 const y = 1000;
-var drawings;
-var thickness;
-var paintColor;
+const radius = 30;
+
+let drawings;
+let rubberPoints;
+let thickness;
+let paintColor;
+let rubber;
+let radiusRubber;
 
 function setup() {
     createCanvas(x, y);
-    drawings   = [];
-    paintColor = chooseColor();
-    thickness  = $("#sliderWidth").val();
+    drawings       = [];
+    rubberPoints = [];
+    paintColor     = chooseColor();
+    thickness      = $("#sliderWidth").val();
+    rubber         = false;
+    radiusRubber   = Math.pow(radius,2);
+    console.log(radiusRubber);
 }
   
 function draw() {
     background(chooseColor($("#background").val()));
-    paintMark();
+    if(!rubber){
+        paintMark();
+    } else {
+        paintRubber();
+        clearFigure();
+    }
     paint();
 }
 
@@ -23,7 +37,34 @@ function paintMark() {
     point(mouseX, mouseY);
 }
 
+function clearFigure() {
+    rubberPoints.forEach(point => {
+        drawings.forEach(element => {
+            for (const elementPoint of element.points) {
+                console.log(
+                    Math.pow((point[0] + elementPoint[0]),2)+ Math.pow((point[1] - elementPoint[1]),2)
+                );
+                if (
+                  Math.pow((point[0] + elementPoint[0]),2) 
+                + Math.pow((point[1] - elementPoint[1]),2) 
+                == radiusRubber) {
+                    element.delete = true;
+                    console.log("coincede");
+                    break;
+                }
+            }
+        });
+    });
+}
+
+function paintRubber() {
+   stroke(220, 0, 220);
+   strokeWeight(10);
+   point(mouseX, mouseY);
+}
+
 function paint() {
+    drawings = drawings.filter(function(element, index, arr){ return !element.delete;})
     drawings.forEach(element => {
         if (element.points.length == 1) {
             const initialPoint = element.points[0]
@@ -44,15 +85,31 @@ function paint() {
 }
 
 function mousePressed(){
-    drawings.push({
-        "points": [[mouseX,mouseY]],
-        "width": thickness,
-        "color": paintColor
-    });
+    if(!rubber){
+        drawings.push({
+            "points" : [[mouseX,mouseY]],
+            "width"  : thickness,
+            "color"  : paintColor,
+            "delete" : false
+        });
+    } else {
+        rubberPoints.push([mouseX,mouseY]);
+    }
 }
 
 function mouseDragged(){
-    drawings[drawings.length - 1].points.push([mouseX,mouseY]);
+    if(!rubber){
+        drawings[drawings.length - 1].points.push([mouseX,mouseY]);
+    } else {
+        rubberPoints.push([mouseX,mouseY]);
+    }
+}
+
+function keyPressed(){
+    if (key=="b" || key=="B") {
+        rubber = !rubber;
+        rubberPoints = [];
+    }
 }
 
 /*function mouseReleased(){
