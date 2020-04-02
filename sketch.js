8e31e6c24b2129919552584ee21707fd1dc5f16e
miguel-kjh <miguelangel.kjh@gmail.com>
2020-04-02 10:31:20 +0100

    rubber done

--- sketch.js
+++ sketch.js
@@ -1,29 +1,20 @@
-const x = 1000;
-const y = 1000;
+const x = $(document).width();
+const y = $(document).height();
 
 let drawings;
-let rubberPoints;
 let thickness;
 let paintColor;
-let rubber;
 
 function setup() {
     createCanvas(x, y);
     drawings       = [];
-    rubberPoints = [];
     paintColor     = chooseColor();
     thickness      = $("#sliderWidth").val();
-    rubber         = false;
 }
   
 function draw() {
     background(chooseColor($("#background").val()));
-    if(!rubber){
-        paintMark();
-    } else {
-        paintRubber();
-        clearFigure();
-    }
+    paintMark();
     paint();
 }
 
@@ -33,52 +24,7 @@ function paintMark() {
     point(mouseX, mouseY);
 }
 
-function isIntersection(ax,ay,bx,by,cx,cy,dx,dy) {
-    firstGrade  = (by-ay)/(bx-ax);
-    secondGrade = (dy-cy)/(dx-cx);
-    if (firstGrade == secondGrade) {
-        return false;
-    }
-    firstCte  = -(firstGrade*ax)  + ay;
-    secondCte = -(secondGrade*cx) + cy;
-
-    mid    = firstGrade-secondGrade; 
-    pointx = (secondCte-firstCte)/mid;
-    pointy = ((firstGrade*secondCte) - (secondGrade*firstCte))/mid;
-
-    return cx <= pointx && cy <= pointy && pointx <= dx && pointy <= dy 
-}
-
-function clearFigure() {
-    drawings.forEach(element => {
-        for (let index = 0; index < element.points.length-1; index++) {
-            console.log(element.delete);
-            if(element.delete) break;
-            const point = element.points[index];
-            const nextPoint = element.points[index+1];
-            for (let indexRubber = 0; indexRubber < rubberPoints.length-1; indexRubber++) {
-                const rubberPoint = rubberPoints[indexRubber];
-                const nextRubberPoint = rubberPoints[indexRubber+1];
-                if(isIntersection(
-                    rubberPoint[0],rubberPoint[1],nextRubberPoint[0],nextRubberPoint[1],
-                    point[0],point[1],nextPoint[0],nextPoint[1]
-                )){
-                    element.delete = true;
-                    break;
-                }
-            }
-        }
-    });
-}
-
-function paintRubber() {
-   stroke(220, 0, 220);
-   strokeWeight(10);
-   point(mouseX, mouseY);
-}
-
 function paint() {
-    drawings = drawings.filter(function(element, index, arr){ return !element.delete;})
     drawings.forEach(element => {
         if (element.points.length == 1) {
             const initialPoint = element.points[0]
@@ -99,30 +45,28 @@ function paint() {
 }
 
 function mousePressed(){
-    if(!rubber){
-        drawings.push({
-            "points" : [[mouseX,mouseY]],
-            "width"  : thickness,
-            "color"  : paintColor,
-            "delete" : false
-        });
-    } else {
-        rubberPoints.push([mouseX,mouseY]);
-    }
+    drawings.push({
+        "points" : [[mouseX,mouseY]],
+        "width"  : thickness,
+        "color"  : paintColor,
+    });
 }
 
 function mouseDragged(){
-    if(!rubber){
-        drawings[drawings.length - 1].points.push([mouseX,mouseY]);
-    } else {
-        rubberPoints.push([mouseX,mouseY]);
-    }
+    drawings[drawings.length - 1].points.push([mouseX,mouseY]);
+}
+
+function deleteElemente() {
+    drawings.pop();
 }
 
 function keyPressed(){
-    if (key=="b" || key=="B") {
-        rubber = !rubber;
-        rubberPoints = [];
+    if (key == "b" || key == "B") {
+        deleteElemente();
+    }
+
+    if(key == "a" || key == "A"){
+        drawings = [];
     }
 }
 
